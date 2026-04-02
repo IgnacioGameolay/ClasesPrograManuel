976e2cfe8399233a1e543c42792c3f38f2b0b76b
Your Name <you@example.com>
2026-04-01 23:32:44 -0300

    Clase 2 (agregando ejemplo con Sobrecarga Constructores): 1/04/2026

---
+++ Clase 2/EjercicioEmpleadoConSobrecarga.java
@@ -0,0 +1,69 @@
+
+class Empleado{
+    protected String nombre;
+    protected int sueldoBase;
+
+    public Empleado(String nombre, int sueldoBase){
+        this.nombre = nombre;
+        this.sueldoBase = sueldoBase;
+    }
+
+    public Empleado(String nombre){
+        this.nombre = nombre;
+    }
+
+
+    public int calcularSueldo(){
+        return sueldoBase;
+    }
+}
+
+class EmpleadoTiempoCompleto extends Empleado{
+    public EmpleadoTiempoCompleto(String nombre, int sueldoBase){
+        super(nombre, sueldoBase);
+    }
+
+    @Override
+    public int calcularSueldo(){
+        return sueldoBase;
+    }
+}
+
+class EmpleadoPorHora extends Empleado{
+    private int horasTrabajadas = 0;
+    private int valorHora = 0;
+    private int sueldoTotal;
+
+    public EmpleadoPorHora(String nombre, int horasTrabajadas, int valorHora){
+        super(nombre);
+        this.horasTrabajadas = horasTrabajadas;
+        this.valorHora = valorHora;
+        this.sueldoTotal = horasTrabajadas*valorHora;
+    }
+
+    @Override
+    public int calcularSueldo(){
+        return sueldoTotal;
+    }
+}
+
+
+public class EjercicioEmpleadoConSobrecarga{
+    public static void main(String[] args){
+        Empleado e1 = new EmpleadoTiempoCompleto("Toy Cansado jefe", 500000);
+        Empleado e2 = new EmpleadoPorHora("Pepe", 30, 30000);
+
+        System.out.println("Yo gano: " + e1.calcularSueldo());
+        System.out.println("Yo gano: " + e2.calcularSueldo());
+    }
+}
+
+
+/*
+
+5. Definir una subclase `EmpleadoPorHora` que:
+   - Herede de `Empleado`
+   - Tenga atributos `horasTrabajadas` (int) y `valorHora` (int)
+   - Sobreescriba el método `calcularSueldo()` para calcular el sueldo en base a estos valores
+
+*/
